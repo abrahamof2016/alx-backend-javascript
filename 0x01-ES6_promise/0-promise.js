795a77f7762aa494d301a8fc582843d251fcd719
abrahamof2016 <jarageda9@gmail.com>
2024-10-29 13:06:03 +0000

    pass linter

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-function getResponseFromAPI() {
-	return new (Promise((resolve, reject) => {
-		setTimeout(() => {
-			const response = "True";
-			resolve(response);
-		}, 1000);
-	}));
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => reslove(true));
 }
-export default getResponseFromAPI;
