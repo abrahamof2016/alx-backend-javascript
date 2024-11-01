059f7af7e806dd0578f1afcc2df4670fdcab42d1
abrahamof2016 <jarageda9@gmail.com>
2024-11-01 06:07:59 +0000

    add 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const file = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, file])
+    .then((val) => val);
+}
