cff24b62c4819bd381392d0feb056d8aa92a6479
abrahamof2016 <jarageda9@gmail.com>
2024-11-06 18:52:59 +0000

    add 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,20 @@
-const getListStudents = () => [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' },
-];
-
-export default getListStudents;
+export default function getListStudents() {
+  const studentList = [];
+  const studentFirst = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const studentSecond = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const studentThird = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  studentList.push(studentFirst, studentSecond, studentThird);
+  return studentList;
+}
