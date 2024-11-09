cb570d4b38b6014aabff42fd9f3c85401eb0ff53
abrahamof2016 <jarageda9@gmail.com>
2024-11-09 14:19:57 +0000

    add 10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+  return map;
+};
+export default updateUniqueItems;
